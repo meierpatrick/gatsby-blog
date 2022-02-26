1baa3f2e2a4014820309901c28319cd0adef82d3
dev <dev@Patricks-MacBook-Pro.local>
2022-02-26 08:15:10 +0200

    Added my project

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,12 @@
 import * as React from "react";
+import Layout from "../layout";
 
 const IndexPage = () => {
   return (
-    <main>
+    <Layout>
       <title>Home Page</title>
       <h1>Hallo Blog</h1>
-    </main>
+    </Layout>
   );
 };
 
